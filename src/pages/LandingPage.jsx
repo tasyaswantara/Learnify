ebe62fed4edbe5416acd8a7885690f0ce21c150d
tasyaswantara <tasyaswantara@gmail.com>
2026-09-04 21:45:11 +0700

    update: remove span placement

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,9 +10,6 @@ function LandingPage() {
       <div className="mx-auto flex min-h-screen max-w-6xl flex-col px-5 py-5 sm:px-8 lg:px-10">
         <header className="flex items-center justify-between">
           <Logo />
-          <span className="hidden text-sm font-semibold text-ink-500 sm:inline">
-            Placement Test
-          </span>
         </header>
 
         <section
